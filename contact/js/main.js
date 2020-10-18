457891074db8a91c758997fddedabbe6ee24e2e0
yoshiishunichi <iihsyo@gmail.com>
2020-10-18 13:04:32 +0900

    menulist complete

---
+++ contact/js/main.js
@@ -0,0 +1,31 @@
+"use strict";
+{
+    const header = document.querySelector("header");
+    const menubtn = document.getElementById("open");
+    const menulist = document.getElementById("menulist");
+    let menuOpen = false;
+    header.style.height = 75 + "px";
+    menubtn.addEventListener("click", () => {
+        if (menuOpen) {
+            header.style.height = 75 + "px";
+            menulist.style.display = "none";
+        }
+        else {
+            header.style.height = 170 + "px";
+            menulist.style.display = "block";
+        }
+        menuOpen = !menuOpen;
+        console("押した");
+    });
+    window.addEventListener("resize", function () {
+        if (window.innerWidth <= 700 && menuOpen) {
+            header.style.height = 170 + "px";
+            menulist.style.display = "block";
+        }
+        else {
+            header.style.height = 75 + "px";
+            menulist.style.display = "none";
+        }
+    }, false);
+
+}
